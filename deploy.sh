9903aa473397eb4c56de5ea14f01ccc7b936f8c7
Brian Cepuran <bcepuran@tlm0-bcepuran.desire2learn.d2l>
2018-07-17 16:18:28 -0400

    updated deploy script to be more descriptive

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,12 @@ cp .path ~
 cp .stCommitMsg ~ 
 
 if [ -f "./env.sh" ]; then sh env.sh && echo "env.sh FOUND - executing bash script"; fi 
-if [ ! -f "env.sh" ]; then echo "env.sh NOT FOUND"; fi
+if [ ! -f "env.sh" ]; then 
+    echo "env.sh NOT FOUND - create one with the following data:"; 
+    echo 'git config --global user.name "YOUR NAME"'
+    echo 'git config --global user.email "YOUR EMAIL"';
+    echo 'echo "finished updating global git user variables"';
+fi
 
 echo ""
 echo "now run the following command to source all the relevant files:"
